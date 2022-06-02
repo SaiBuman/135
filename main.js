8fd31c5792a1f8edf5c830140e791dc485b95820
Sai Buman <78092854+SaiBuman@users.noreply.github.com>
2022-06-02 19:33:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 status1 = "" ;
+object = [] ;
 function setup() {
     canvas = createCanvas(480,380);
  }
@@ -8,9 +9,7 @@ function setup() {
      video.hide();
  }
 
- function draw() {
-     image(video, 0,0,480,380);
- }
+
 
  function start() {
      objectDetecter = ml5.objectDetector("cocossd",modelLoaded);
@@ -24,3 +23,32 @@ function setup() {
      video.speed(1);
      video.volume(1);
  }
+
+ function gotresult(error,result) {
+      if (error) {
+          console.error(error);
+      }
+
+      else {
+          console.log(result);
+          object = result ;
+      }
+ }
+
+ function draw() {
+    image(video, 0,0,480,380);
+
+    if (status1 != " ") {
+        objectDetecter.detect(video,gotresult);
+        for (let i = 0; i < object.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected" ; 
+            document.getElementById("object").innerHTML = "Objects found : " + object.length;
+            percent = floor(object[i].confidence*100);
+            fill("#4961e1");
+            text(object[i].label + " " + percent + " %" , object[i].x + 15, object[i].y + 15);
+            nofill();
+            stroke("#4961e1");
+            rect(object[i].x , object[i].y , object[i].width ,object[i].height);
+        }
+    }
+}
